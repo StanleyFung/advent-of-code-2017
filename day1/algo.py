abc9dd8c564b88df48e4424cd14094f638ce91e1
StanleyFung <stanleyfung94@hotmail.com>
2017-12-02 21:45:49 -0500

    Part Two

--- day1/algo.py
+++ day1/algo.py
@@ -3,27 +3,27 @@ def getInput():
     return open("input", "r").read()
 
 # Personal test answers
-def getCorrectAnswers():
-    answerFile=open("tests-answer", "r")
+def getCorrectAnswers(part):
+    answerFile=open(part + "/tests-answer", "r")
     answers = []
     for line in answerFile:
         answers.append(int(line))
     return answers
 
 # Personal tests
-def getTests():
-    testFile=open("tests", "r")
+def getTests(part):
+    testFile=open(part + "/tests", "r")
     tests = []
     for line in testFile:
         tests.append(line.rstrip())
     return tests
 
-def personalTests():
-    testInputs = getTests()
-    correctAnswers = getCorrectAnswers()
+def personalTests(part, operation):
+    testInputs = getTests(part)
+    correctAnswers = getCorrectAnswers(part)
     for indx, test in enumerate(testInputs):
         print("Test " + str(indx+1))
-        answer = partOne(test)
+        answer = operation(test)
         correct = correctAnswers[indx]
 
         if answer == correct:
@@ -43,7 +43,25 @@ def partOne(inputString):
             accum += int(current)
     return accum
 
-personalTests()
+def partTwo(inputString):
+    accum = 0
+    halfway = int(len(inputString) / 2)
+    for i in range(0, len(inputString)):
+        current = inputString[i]
+        nextPlusHalfWay = i + halfway
+        nextIndex = nextPlusHalfWay % len(inputString)
+        next = inputString[nextIndex]
+        if current == next:
+            accum += int(current)
+    return accum
+
+personalTests("partOne", partOne)
+personalTests("partTwo", partTwo)
 print()
 print("Input Result")
+
+print("Part One")
 print(partOne(getInput()))
+
+print("Part Two")
+print(partTwo(getInput()))
