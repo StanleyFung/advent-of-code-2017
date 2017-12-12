a6ab72bb2951692a7bc6e4e81fbca923e0ffa353
StanleyFung <stanleyfung94@hotmail.com>
2017-12-12 17:41:11 -0500

    Day 12

---
+++ day11/hex.py
@@ -0,0 +1,14 @@
+def getInput(path):
+    file = open(path, "r")
+    input = file.readlines().pop().split(",")
+    print(input)
+    return input
+
+def hex(input):
+    """
+        If previous direction was sw, the only 
+    """
+    distance = 0
+    
+
+getInput("day11/input")
